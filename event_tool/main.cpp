996e217cd2752d23b354a86c353bbdbc673a0347
jhonconal <jhonconal@outlook.com>
2026-03-31 22:04:47 +0800

    Fix AppName(Event Tool)

--- event_tool/main.cpp
+++ event_tool/main.cpp
@@ -11,7 +11,7 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
-    QCoreApplication::setApplicationName("cmdtool");
+    QCoreApplication::setApplicationName("Event Tool");
     QCoreApplication::setApplicationVersion("1.0");
 
     QCommandLineParser parser;
